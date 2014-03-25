b288257b9f662016dfdb242078f8cf423c2d070f
jongrover <hello@jonathangrover.com>
2014-03-25 12:07:24 -0400

    update solution in js file

--- js/flickrSearch.js
+++ js/flickrSearch.js
@@ -1,57 +1,74 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  
+  // Clears results on typing serach term.
 
-  $('#keyword').focus(function() {
-    $('#search').val('');
-    $('#feed').empty(); //erase last search
+  $('input#keyword').focus(function(){
+    $('#feed').html('');  
   });
+  
+  // Search flicker API
 
-
-  //user clicks button (event listener)
-  $('#search').click(function(){ 
-
-    //get search term
-    var keyword = $('#keyword').val();
-
-    //include search term into AJAX request URL
-    var url = 'http://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=2fd41b49fedfd589dc265350521ab539&tags='+keyword+'&jsoncallback=?';
-
-    //make AJAX request
-    $.getJSON(url, function(data) {
-
-      console.log(data);
-
-      $.each(data.photos.photo, function(i, photo) {
-
-        var content = '<img src="http://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'.jpg">';
-
-        $('#feed').append(content);
-
-      });
+  $('button#search').click(function(){
+    var keyw = $('input#keyword').val(); //retrieves keyword(s) typed.
+            
+    $.getJSON('http://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=2fd41b49fedfd589dc265350521ab539&tags='+keyw+'&jsoncallback=?',function(data){ //display JSON feed using keyword(s) typed
     
-    });
-
-  });
-
-
-    //build the image urls from the data
-
-    //render in the document
-
-
+      console.log(data); //see if results came in...
+    
+      $.each(data.photos.photo, function(i,photo){
+      
+        content = '<a href="http://www.flickr.com/photos/'+photo.owner+'/" target="_blank"><img src="http://farm'+photo.farm+'.staticflickr.com/'+photo.server+'/'+photo.id+'_'+photo.secret+'.jpg"></a>';     
+        $(content).appendTo("#feed"); //displays each result inside feed div above.                  
+      });
+    });   
+  }); 
+    
 });
 
 /*
 
-Flickr API url: http://www.flickr.com/services/api/request.rest.html
+API url: 
 
+http://www.flickr.com/services/api/request.rest.html
 
-Example AJAX request URL with tags=cat:
+AJAX request URLwith tags=cat (search term = cat):
 
 http://api.flickr.com/services/rest/?format=json&method=flickr.photos.search&api_key=2fd41b49fedfd589dc265350521ab539&tags=cat&jsoncallback=?
 
+JSON Snippet:
 
-Info about creating photo url from json data: http://www.flickr.com/services/api/misc.urls.html
+jsonFlickrApi({
+    "photos": {
+        "page": 1,
+        "pages": 46641,
+        "perpage": 100,
+        "total": "4664056",
+        "photo": [
+            {
+                "id": "7790251192",
+                "owner": "80992738@N00",
+                "secret": "50b0af1b38",
+                "server": "8440",
+                "farm": 9,
+                "title": "Friends",
+                "ispublic": 1,
+                "isfriend": 0,
+                "isfamily": 0
+            },
 
-Example constructing photo url: http://farm{farm#}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+object property path: 
+
+photos.photo.farm
+photos.photo.server
+photos.photo.id
+photos.photo.secret
+
+info about creating photo url from son data: http://www.flickr.com/services/api/misc.urls.html
+
+http://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+Example Test:
+
+http://farm9.staticflickr.com/8440/7790251192_50b0af1b38.jpg
 
 */
